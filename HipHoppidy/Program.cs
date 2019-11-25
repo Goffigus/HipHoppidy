2ce5b303f24856f4ed77ab26a1a18f97b194f73e
Goffigus <56737195+Goffigus@users.noreply.github.com>
2019-11-25 11:34:43 -0500

    Add project files.

---
+++ HipHoppidy/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HipHoppidy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hoppidy();
+            //HipHoppidy();
+            string input = Console.ReadLine();
+            int f = Convert.ToInt32(input);
+
+            Console.WriteLine("The factorial is {0}", fact(f));
+        }
+
+        private static void Hoppidy()
+        {
+            Console.WriteLine("Hoppidy");
+        }
+
+        private static void HipHoppidy()
+        {
+            for(int x = 0; x < 10; ++x)
+            {
+                Hoppidy();
+            }
+            HipHoppidy(); //calls on itself causing infinite recursion, NOT GOOD
+        }
+
+        private static int fact(int f)
+        {
+            if(f <= 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return f * fact(f - 1);
+            }
+        }
+    }
+}
